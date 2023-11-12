7579ccec6c5092cad407f23d62f0bb3451990f4f
synoet <teonys@nyu.edu>
2023-11-12 17:44:26 -0500

    FIX supervisord to run install extension before

--- theia-pincer/install_extension.sh
+++ theia-pincer/install_extension.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-curl https://pincer-server.fly.dev/version/latest/download -o /opt/code-server/pincer.vsix 
-code-server --install-extension /opt/code-server/pincer.vsix 
+curl  https://pincer-server.fly.dev/version/latest/download -o /opt/code-server/pincer.vsix 
+/usr/bin/code-server --user-data-dir=/opt/code-server --install-extension /opt/code-server/pincer.vsix 

05bf9cc80de36b3af451a415747cc33ace03d88d
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2023-12-07 17:05:56 -0500

    CHG clone study repo on pincer IDE start

--- theia-pincer/install_extension.sh
+++ theia-pincer/install_extension.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 curl  https://pincer-server.fly.dev/version/latest/download -o /opt/code-server/pincer.vsix 
 /usr/bin/code-server --user-data-dir=/opt/code-server --install-extension /opt/code-server/pincer.vsix 
+
+if [ ! -d /home/anubis/2023_study ]; then
+    git clone https://github.com/GusSand/2023_study.git
+fi
+
+exit 0

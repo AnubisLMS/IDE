3ff6285b299a9a6b6242cd93e782fa5e6df02b36
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2023-12-07 17:14:14 -0500

    CHG fix spacing

--- theia-pincer/install_extension.sh
+++ theia-pincer/install_extension.sh
@@ -1,9 +1,12 @@
-#! /bin/bash
+#!/bin/bash
+
+set -xe
+
+# Install latest pincer extension
 curl  https://pincer-server.fly.dev/version/latest/download -o /opt/code-server/pincer.vsix 
 /usr/bin/code-server --user-data-dir=/opt/code-server --install-extension /opt/code-server/pincer.vsix 
 
+# Download study base files if not already there
 if [ ! -d /home/anubis/2023_study ]; then
     git clone https://github.com/GusSand/2023_study.git
 fi
-
-exit 0

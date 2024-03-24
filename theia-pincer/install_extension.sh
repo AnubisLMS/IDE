743190a211dc5b517dd191cf051c2abbfe5a1d98
synoet <teonys@nyu.edu>
2024-03-24 19:32:02 -0400

    CHG update pincer extension download url

--- theia-pincer/install_extension.sh
+++ theia-pincer/install_extension.sh
@@ -3,7 +3,7 @@
 set -xe
 
 # Install latest pincer extension
-curl  https://pincer-server.fly.dev/version/latest/download -o /opt/code-server/pincer.vsix 
+curl  https://pincer.anubis-lms.io/version/latest/download -o /opt/code-server/pincer.vsix 
 /usr/bin/code-server --user-data-dir=/opt/code-server --install-extension /opt/code-server/pincer.vsix 
 
 # Download study base files if not already there
